8e1cf37a0ea0253b45074c89b1664a4da72007fb
Chinenkova Daria <belekhova.daria@gmail.com>
2018-07-31 18:00:19 +0300

    render board

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,24 +3,11 @@ import './Cell.css';
 class Cell extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            alive: false,
-        }
-        this.changeState = this.changeState.bind(this);
-    }
-
-    changeState() {
-        this.setState(prevState => ({
-            alive: !prevState.alive
-        }));
-        }
-    
+    }   
 
     render() {
         return (
-        <div>
-            <div className={`Cell ${this.state.alive ? 'alive' : ''}`} onClick={this.changeState} />            
-        </div>
+            <td className={`Cell ${this.props.alive ? 'alive' : ''}`} onClick={this.props.changeState} />
         );
     }
     }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Game.css';
-import Row from './Row';
+//import Row from './Row';
+import Cell from './Cell';
 class Game extends React.Component {
     constructor() {
         super();
@@ -10,7 +11,8 @@ class Game extends React.Component {
             cells: [],
         }
         this.CreateCells = this.CreateCells.bind(this);
-        this.RenderRows = this.RenderRows.bind(this);
+        this.changeState = this.changeState.bind(this);
+        //this.RenderRows = this.RenderRows.bind(this);
     }
 
     componentWillMount() {
@@ -20,27 +22,47 @@ class Game extends React.Component {
     CreateCells() {
         for (let y = 1; y <= this.state.rows; y++) {
             for (let x = 1; x <= this.state.columns; x++) {
-                this.setState(prevState => ({cells: [...prevState.cells, {x, y}]}));
+                let alive = false;
+                this.setState(prevState => ({cells: [...prevState.cells, {x, y, alive}]}));
             }
         }
     }
 
-    RenderRows() {        
-        for(let row=1; row<=this.state.rows; row++) {
-            let rowCells = this.state.cells.filter(cell => cell.y === row)
-            return (                
-                <Row rowCells={rowCells} id={row}/>
-            )
-        }
+    changeState() {
+        this.setState(prevState => ({
+            alive: !prevState.alive
+        }));
     }
+
+    renderBoard = () => {
+        let board = []    
+        // Outer loop to create parent
+        for (let row = 1; row <= this.state.rows; row++) {
+            let children = []
+            //Inner loop to create children
+            for (let c = 1; c <= this.state.columns; c++) {
+                children.push(<Cell x={c} y={row} changeState={this.changeState}/>)
+            }
+            //Create the parent and add the children
+            board.push(<tr>{children}</tr>)
+        }
+            return board;
+        }
+
+    // RenderRows() {
+    //     for(let row=1; row<=this.state.rows; row++) {
+    //         let rowCells = this.state.cells.filter(cell => cell.y === row)
+    //         return <Row rowCells={rowCells} id={row}/>;
+    //     }
+    // }
 
 
     render() {
         return (
         <div>
-            <div className="Board">
-                {this.RenderRows()}
-            </div>
+            <table className="Board">
+                {this.renderBoard()}
+            </table>
         </div>
         );
     }
